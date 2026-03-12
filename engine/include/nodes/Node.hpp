a4c569e40d4eff1460be81a5f48886ece03fbf05
LeoschDD <leoschdd@gmail.com>
2026-03-12 19:55:35 +0100

    defined node operations

--- engine/include/nodes/Node.hpp
+++ engine/include/nodes/Node.hpp
@@ -4,53 +4,77 @@
 
 namespace rle
 {
+    class SceneManager;
+
     class Node
     {
     private:
+        friend class SceneManager;
+
         static uint32_t next_id_;
         uint32_t id_;
         std::string name_;
 
+        bool active_{true};
+        bool in_tree_{false};
+        bool ready_{false};
+        bool processing_{false};
+
         Node* parent_{nullptr};
         std::vector<std::unique_ptr<Node>> children_;
 
-        // std::vector<std::function<void>> queued_operations_
+        std::vector<std::function<void>> queued_operations_;
     
     private:
-        // EnterTree
-        // ExitTree
-        // Ready
+        void EnterTree();
+        void ExitTree();
+        void Input();
+        void Update(const float dt);
+        void Render();
 
-        // Input
-        // Update
-        // Render
+        [[nodiscard]] bool HasAncestor(const Node* ancestor) const;
+        void FlushQueuedOperations();
     
     protected:        
-        // OnEnterTree
-        // ...
+        virtual void EnterTreeInternal() {}
+        virtual void ExitTreeInternal() {}
+        virtual void ReadyInternal() {}
+        virtual void InputInternal() {}
+        virtual void UpdateInternal(const float dt) {}
+        virtual void RenderInternal() {}
 
+        virtual void OnEnterTree() {}
+        virtual void OnExitTree() {}
+        virtual void OnReady() {}
+        virtual void OnInput() {}
+        virtual void OnUpdate(const float dt) {}
+        virtual void OnRender() {}
+        
     public:
         Node();
         Node(const std::string& name);
         virtual ~Node() = default;
 
+        Node(const Node&) = delete;
+        Node& operator=(const Node&) = delete;
+        Node& operator=(Node&&) = delete;
+
         virtual void Serialize(nlohmann::ordered_json& json) const;
         virtual void Deserialize(const nlohmann::ordered_json& json);
 
         void AddChild(std::unique_ptr<Node> child);
-        // RemoveChild
-        // DetachChild
-        // Reparent
+        void RemoveChild(Node* child);
+        void Reparent(Node* parent);
+        std::unique_ptr<Node> DetatchChild(Node* child);
 
-        // AddChildQueued
-        // RemoveChildQueued
-        // DetachChildQueued
-        // ReparentQueued
+        void AddChildQueued(std::unique_ptr<Node> child);
+        void RemoveChildQueued(Node* child);
+        void ReparentQueued(Node* parent);
 
-        // QueueFree
-
-        // node* GetChild(id)
-        // node* GetChild(name)
+        Node* GetChild(const std::string& name);
+        Node* GetChild(uint32_t id);
+        Node* GetChildRecursive(const std::string& name);
+        Node* GetChildRecursive(uint32_t id);
 
         uint32_t GetId() const {return id_;}
         const std::string& GetName() const {return name_;}
--- engine/include/scene/SceneManager.hpp
+++ engine/include/scene/SceneManager.hpp
@@ -11,17 +11,15 @@ namespace rle
         std::unique_ptr<Scene> active_scene_{nullptr};
         SceneSerializer serializer_;
 
-        std::string startup_scene_path_;
-
     public:
         explicit SceneManager(NodeRegistry* node_registry);
 
         bool LoadScene(const std::string& path);
         void SaveScene(const std::string& path);
+
         void CreateDefaultScene() {active_scene_ = std::make_unique<Scene>();}
 
         void SetScene(std::unique_ptr<Scene> scene) {active_scene_ = std::move(scene);}
         Scene* GetScene() {return active_scene_.get();}
-        
     };
 }
